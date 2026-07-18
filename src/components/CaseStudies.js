078bca246786ec4dcbfdcd5273472b6fed099bd4
Zachary Djerdjouri <zdjer050@uottawa.ca>
2026-07-17 20:00:22 -0400

    added link and images

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -1,30 +1,36 @@
+import barberImg from '../assets/barber.png';
+import shopImg from '../assets/eshop.png';
+import memoryImg from '../assets/memory.png';
+import dashboardImg from '../assets/dashboard.png';
+
 const projects = [
     {
       title: 'Design 1: Service Website',
       description: 'A responsive website for a fake business.',
       image:
-        'https://images.unsplash.com/photo-1502945015378-0e284ca1a5be?q=80&w=1470&auto=format&fit=crop',
+        barberImg,
       link: 'https://supercutz.netlify.app'
     },
     {
       title: 'Design 2: Memory Game.',
       description:'Interactive game built for the web',
       image:
-        'https://images.unsplash.com/photo-1541278107931-e006523892df?q=80&w=1471&auto=format&fit=crop',
+        memoryImg,
         link: 'https://think-tiles.netlify.app'
     },
     {
       title: 'Design 3: E-commerce site',
       description: 'Modern e-commerce shopping experience.',
       image:
-        'https://plus.unsplash.com/premium_photo-1683746792239-6ce8cdd3ac78?q=80&w=687&auto=format&fit=crop',
+        shopImg,
         link: 'https://chronos-e-commerce.netlify.app'
     },
     {
       title: 'Design 4: Analytics Dashboard',
       description: 'Dashboard with analytics and visualizations.',
       image:
-      'https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?q=80&w=1476&auto=format&fit=crop'
+      dashboardImg,
+      link: 'https://rink-metrics.netlify.app'
     }
   ];
   
